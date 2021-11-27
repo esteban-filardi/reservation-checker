3ee4aa6bea6f90f818f272faceec0d5af840e34b
Esteban Filardi <esteban.filardi@gmail.com>
2021-11-27 19:04:13 -0300

    Set up application hosted service

---
+++ ReservationChecker/AplicationHostedService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservationChecker;
+
+public class AplicationHostedService : IHostedService
+{
+    private readonly ILogger _logger;
+    private readonly IHostApplicationLifetime _appLifetime;
+
+    public AplicationHostedService(
+        ILogger<AplicationHostedService> logger,
+        IHostApplicationLifetime appLifetime)
+    {
+        _logger = logger;
+        _appLifetime = appLifetime;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _appLifetime.ApplicationStarted.Register(async () =>
+        {
+            try
+            {
+                _logger.LogInformation($"Starting");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Unhandled exception");
+            }
+            finally
+            {
+                // Stop the application once the work is done
+                _appLifetime.StopApplication();
+            }
+        });
+
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
---
+++ ReservationChecker/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ReservationChecker;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        using IHost host = CreateHostBuilder(args).Build();
+
+        await host.RunAsync();
+    }
+
+    static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+        .ConfigureServices((hostContext, services) =>
+        {
+            services.AddHostedService<AplicationHostedService>();
+        });
+}
